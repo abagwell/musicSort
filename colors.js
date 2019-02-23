d45d33e5083527ebf0c381c2daa7b0a1a342767c
Andrew Bagwell <andrewbagwell1@gmail.com>
2019-02-23 16:32:28 -0600

    added ability to color table

---
+++ colors.js
@@ -0,0 +1,13 @@
+//this class contains the colors that are used to color the table
+
+var brown = [];
+var blue = [];
+var green = [];
+var orange = [];
+var pink = ['#FFC0CB', '#FFB6C1', '#FF69B4', '#FF1493', '#DB7093', '#C71585'];
+var purple = ['#E6E6FA', '#D8BFD8', '#DDA0DD', '#DA70D6', '#EE82EE', '#FF00FF', '#BA55D3', '#9932CC', 
+'#9400D3', '#8A2BE2', '#8B008B', '#800080', '#9370DB', '#7B68EE', '#6A5ACD', '#483D8B', '#663399', '#4B0082'];
+var red = ["#FFA07A", "#FA8072", "#E9967A", "#F08080", "#CD5C5C", "#DC143C", "#FF0000", "#B22222", "#8B0000"];
+var yellow = [];
+
+
